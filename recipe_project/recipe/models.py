e199912b642c4196c4fd23c7a81af7b75c20f846
odhisbrian43 <odhisbrian43@gmail.com>
2025-01-08 11:04:08 +0300

    Modified category model

--- recipe_project/recipe/models.py
+++ recipe_project/recipe/models.py
@@ -3,6 +3,9 @@ from accounts.models import CustomUser
 import jsonfield
 from django.core import validators
 
+#Category model.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
 
 #Recipe model.
 class Recipe(models.Model):
@@ -11,11 +14,11 @@ class Recipe(models.Model):
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, )
     description = models.TextField()
     ingredients = jsonfield.JSONField(default=[], null=True)
-    category = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='category')
     instructions = models.TextField()
     preparation_time = models.PositiveIntegerField
     cooking_time = models.PositiveIntegerField
-    servings = models.PositiveBigIntegerField
+    servings = models.PositiveIntegerField
     created_date = models.DateField(auto_now_add=True)
 
     #Recipe instance string representation.
